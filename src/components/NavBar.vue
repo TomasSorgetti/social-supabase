46dfac1aa05ec9068104d5878fc4b7a48acbddae
TomasSorgetti <tomassorgetti456@gmail.com>
2025-09-19 10:24:01 -0300

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <nav>
+      <router-link to="/">Logo</router-link>
+
+      <ul>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/">Chat</router-link>
+        </li>
+        <li>
+          <router-link to="/">Blog</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
