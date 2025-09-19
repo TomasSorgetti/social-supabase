4c0a9f06b8813d1235f4f78debb1e1fd412edae1
TomasSorgetti <tomassorgetti456@gmail.com>
2025-09-19 20:28:46 -0300

    feat: circuit images added

---
+++ src/components/sections/home/Circuit.vue
@@ -0,0 +1,15 @@
+<script setup>
+import circuit from "../../../assets/images/lines.webp";
+import feed from "../../../assets/images/feed.webp";
+</script>
+
+<template>
+  <div class="w-full h-full -translate-y-30">
+    <img :src="circuit" alt="" class="w-full h-auto" />
+    <img
+      :src="feed"
+      alt=""
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-auto"
+    />
+  </div>
+</template>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HomeHero from "../components/sections/home/HomeHero.vue";
+import Circuit from "../components/sections/home/Circuit.vue";
 </script>
 
 <template>
   <main>
     <HomeHero />
+    <Circuit />
   </main>
 </template>
