480e62b4fe904f40cf31a4f5a0d93552dcf57923
TomasSorgetti <tomassorgetti456@gmail.com>
2025-10-06 16:02:53 -0300

    feat: footer added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import Footer from "./components/Footer.vue";
 import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
   <NavBar />
   <router-view />
+  <Footer />
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<script>
+import NerdFeed from "../assets/NerdFeedFooter.webp";
+
+export default {
+  name: "Footer",
+
+  data() {
+    return {
+      NerdFeed,
+    };
+  },
+};
+</script>
+
+<template>
+  <footer class="p-4 lg:px-0 border-t border-border">
+    <div class="container mx-auto">
+      <img
+        :src="NerdFeed"
+        alt="nerd feed"
+        draggable="false"
+        loading="lazy"
+        class="w-full h-auto my-12"
+      />
+      <p class="text-center text-font-secondary border-t border-border pt-4">
+        &copy;Copyright 2025
+      </p>
+    </div>
+  </footer>
+</template>
